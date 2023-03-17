6ad3eead10f883e4ea570b7850b6b0f03981d3d4
boring-km <kms0644804@naver.com>
2023-03-18 01:04:36 +0900

    feat: 좀 더 디테일하게 보여주기

--- app/src/main/java/com/boringkm/customcalendar/MainActivity.kt
+++ app/src/main/java/com/boringkm/customcalendar/MainActivity.kt
@@ -3,14 +3,22 @@ package com.boringkm.customcalendar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.boringkm.customcalendar.ui.theme.CustomCalendarTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,8 +38,38 @@ class MainActivity : ComponentActivity() {
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        Text(text = "${monthCalendar.year}년 ${monthCalendar.month}월 ${monthCalendar.date}일")
+                        Text(text = "오늘 ${monthCalendar.today.year}년 ${monthCalendar.today.month}월 ${monthCalendar.today.day}일")
+                        Spacer(modifier = Modifier.height(20.dp))
+                        val days = monthCalendar.getWeekDays()
+
+                        Row(Modifier.fillMaxWidth().padding(horizontal = 8.dp), horizontalArrangement = Arrangement.SpaceBetween) {
+                            Text(text = "월", Modifier.padding(12.dp), textAlign = TextAlign.Center)
+                            Text(text = "화", Modifier.padding(12.dp), textAlign = TextAlign.Center)
+                            Text(text = "수", Modifier.padding(12.dp), textAlign = TextAlign.Center)
+                            Text(text = "목", Modifier.padding(12.dp), textAlign = TextAlign.Center)
+                            Text(text = "금", Modifier.padding(12.dp), textAlign = TextAlign.Center)
+                            Text(text = "토", Modifier.padding(12.dp), textAlign = TextAlign.Center)
+                            Text(text = "일", Modifier.padding(12.dp), textAlign = TextAlign.Center)
+                        }
+
+                        LazyVerticalGrid(columns = GridCells.Fixed(7)) {
+                            items(days) { date ->
+                                Box(modifier = Modifier
+                                    .clip(CircleShape)
+                                    .padding(8.dp)) {
+                                    Text(
+                                        text = "${date.day}",
+                                        Modifier
+                                            .fillMaxSize()
+                                            .background(if (date.today) Color.Green else Color.LightGray)
+                                            .padding(8.dp),
+                                        textAlign = TextAlign.Center
+                                    )
+                                }
+                            }
+                        }
                     }
+
                 }
             }
         }
--- app/src/main/java/com/boringkm/customcalendar/MonthCalendar.kt
+++ app/src/main/java/com/boringkm/customcalendar/MonthCalendar.kt
@@ -1,26 +1,59 @@
 package com.boringkm.customcalendar
 
 import android.util.Log
+import com.boringkm.customcalendar.model.CalendarDay
 import java.util.Calendar
 import java.util.Date
 
 
 class MonthCalendar {
 
-    private val calendar = Calendar.getInstance()
-    var year: Int
-    var month: Int
-    var date: Int
+    var lastDayOfMonth: Int
+
+    var today: CalendarDay
 
     init {
+        val calendar = Calendar.getInstance()
         val now = Date()
         calendar.time = now
 
-        year = calendar.get(Calendar.YEAR)
-        month = calendar.get(Calendar.MONTH) + 1
-        date = calendar.get(Calendar.DATE)
+        today = CalendarDay(
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH) + 1,
+            calendar.get(Calendar.DATE),
+            true
+        )
 
-        val maximum = calendar.getMaximum(Calendar.MONTH)
-        Log.i("Calendar", "$maximum")
+        lastDayOfMonth = calendar.getMaximum(Calendar.DAY_OF_MONTH)
+    }
+
+    fun getWeekDays(): MutableList<CalendarDay> {
+        val dayList = mutableListOf<CalendarDay>()
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.DAY_OF_MONTH, 1)
+        val month = calendar.get(Calendar.MONTH) + 1
+        for (i in 0..5) {
+            for (j in 0..6) {
+                calendar.add(Calendar.DAY_OF_MONTH, (1 - calendar.get(Calendar.DAY_OF_WEEK)) + j)
+
+                dayList.add(
+                    CalendarDay(
+                        year = calendar.get(Calendar.YEAR),
+                        month = calendar.get(Calendar.MONTH) + 1,
+                        day = calendar.get(Calendar.DAY_OF_MONTH),
+                        today = today.isToday(calendar)
+                    )
+                )
+
+            }
+
+            val lastDay = dayList.last()
+            Log.i("check", "${lastDay.month}월 ${lastDay.day}일")
+            if (lastDay.month != month) {
+                break
+            }
+            calendar.add(Calendar.WEEK_OF_MONTH, 1)
+        }
+        return dayList
     }
 }
